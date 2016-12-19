554c289ffb6a8308b0ae27d78f3547a02c99023d
Marcelo <marcelomcl_10@hotmail.com>
2016-12-19 01:34:43 -0200

    cria banco atraves do liquibase

--- src/main/java/br/gov/sp/fatec/mapskills/main/Main.java
+++ src/main/java/br/gov/sp/fatec/mapskills/main/Main.java
@@ -1,26 +1,33 @@
 package br.gov.sp.fatec.mapskills.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import br.gov.sp.fatec.mapskills.application.MapSkillsException;
+import br.gov.sp.fatec.mapskills.config.SpringContextConfiguration;
+import br.gov.sp.fatec.mapskills.domain.institution.Course;
+import br.gov.sp.fatec.mapskills.domain.institution.CoursePeriod;
+import br.gov.sp.fatec.mapskills.domain.institution.InstitutionService;
+import br.gov.sp.fatec.mapskills.domain.user.AcademicRegistry;
+import br.gov.sp.fatec.mapskills.domain.user.Student;
+
 public class Main {
 	
-	public static void main(String[] args) {
-		/*@SuppressWarnings("resource")
+	public static void main(String[] args) throws MapSkillsException {
+		@SuppressWarnings("resource")
 		final ApplicationContext context = new AnnotationConfigApplicationContext(SpringContextConfiguration.class);
 		
-		final SkillService service = (SkillService) context.getBean("skillService");
-		final Skill skill = service.findById(1);
-		System.out.println(String.format("old description: %s", skill.getType()));
-		skill.changeType("Resiliencia");
-		skill.changeDescription("Breve descrição da habilidade");
-		service.create(skill);
-		System.out.println(String.format("new description: %s", service.findById(1).getType()));*/
-		final String ra = "1460281423023";
-		System.out.println(ra.length() != 13);
-		System.out.println("fatec "+ ra.substring(0, 3));
-		System.out.println("curso "+ ra.substring(3, 6));
-		System.out.println("ano "+ ra.substring(6, 8));
-		System.out.println("semestre "+ ra.substring(8, 9));
-		System.out.println("aluno "+ ra.substring(9));
+		final InstitutionService institutionService = (InstitutionService) context.getBean("institutionService");
+/*		INSTITUIÇÃO CADASTRADA
+		final Mentor mentor = new Mentor("Mentor Responsavel Teste", "mentor@fatec", "mudar@123");
+		final Institution fatec = new Institution("146", "123456789000", "Jessen Vidal", "São José", mentor);
+		institutionService.saveInstitution(fatec);
+*/	
+		final Course courseA = new Course("028", "Estruturas Leves", CoursePeriod.NOTURNO, "146");
+		institutionService.saveCourse(courseA);
 		
+		final Student studentE = new Student(new AcademicRegistry("1460281423050", "146", "028"), "Student MockE", "1289003400", "studentE@fatec.sp.gov.br", "mudar@123");
+		institutionService.saveStudent(studentE);
 
 	}
 
