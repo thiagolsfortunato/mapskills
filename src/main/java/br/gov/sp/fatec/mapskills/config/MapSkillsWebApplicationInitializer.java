3046196e438ded25db6d271781d02872123ee613
Marcelo <marcelomcl_10@hotmail.com>
2017-02-16 20:54:41 -0200

    add implements webApplicationInitializer

--- src/main/java/br/gov/sp/fatec/mapskills/config/MapSkillsWebApplicationInitializer.java
+++ src/main/java/br/gov/sp/fatec/mapskills/config/MapSkillsWebApplicationInitializer.java
@@ -10,6 +10,7 @@ import javax.servlet.Filter;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
@@ -18,7 +19,8 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
  * do contexto do Spring na aplicação.
  *
  */
-public class MapSkillsWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class MapSkillsWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer implements 
+	WebApplicationInitializer {
 
 	/** {@inheritDoc} */
 	@Override
